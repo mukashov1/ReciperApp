55b91dca773d1d84d740260c3fe9fcddddb35df3
DosyaSDU <doszhan_turar@mail.ru>
2022-11-04 03:03:51 +0600

    FirstPageDone

--- app/src/main/java/com/example/reciper/MainActivity.kt
+++ app/src/main/java/com/example/reciper/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.reciper
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
