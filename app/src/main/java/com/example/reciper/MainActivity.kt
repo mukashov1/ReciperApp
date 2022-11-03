c08b894dda90c7c10bb97f923b802375522168cf
DosyaSDU <doszhan_turar@mail.ru>
2022-11-04 05:52:05 +0600

    Starting Point

--- app/src/main/java/com/example/reciper/MainActivity.kt
+++ app/src/main/java/com/example/reciper/MainActivity.kt
@@ -1,22 +1,14 @@
 package com.example.reciper
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log.d
 import android.widget.Button
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.doszhan_second_page)
-        val check:Button = findViewById(R.id.button)
-        check.setOnClickListener() {
-            val toast = Toast.makeText(this ,"Something",Toast.LENGTH_SHORT)
-            toast.show()
-        }
+        setContentView(R.layout.activity_main)
     }
     // Baha durys bop tur ma?
     // durystap isteik dalwe
