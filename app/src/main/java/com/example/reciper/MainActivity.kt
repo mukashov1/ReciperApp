ece94a48d5bef1f7d987ac855bf019a401c802e6
mukashov1 <78909917+mukashov1@users.noreply.github.com>
2022-11-06 06:42:12 +0600

    creating navbar

--- app/src/main/java/com/example/reciper/MainActivity.kt
+++ app/src/main/java/com/example/reciper/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log.d
 import android.widget.Button
+import androidx.fragment.app.Fragment
 import com.example.reciper.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +16,11 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
-    // Baha durys bop tur ma?
-    // durystap isteik dalwe
+
+    private fun replaceFragment(fragment: Fragment) {
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.button2)
+    }
 }
