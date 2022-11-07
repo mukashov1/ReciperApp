70b1adbbcd3930e29a91c6479ea641681d2e0957
mukashov1 <78909917+mukashov1@users.noreply.github.com>
2022-11-08 03:06:38 +0600

    setting navbar replacement

--- app/src/main/java/com/example/reciper/MainActivity.kt
+++ app/src/main/java/com/example/reciper/MainActivity.kt
@@ -5,12 +5,37 @@ import android.os.Bundle
 import android.util.Log.d
 import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.replace
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.doszhan_log_in_page)
+        setContentView(R.layout.activity_main)
+
+        val navBar = findViewById<BottomNavigationView>(R.id.navBar)
+        navBar.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> replaceFragment(home())
+                R.id.search -> replaceFragment(SearchFragment())
+                R.id.profile -> replaceFragment(profile())
+                else -> {
+
+                }
+            }
+
+            true
+        }
+
     }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout , fragment)
+        fragmentTransaction.commit()
+    }
+
 }
