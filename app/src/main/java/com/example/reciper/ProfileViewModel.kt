676831269c4067869c10773e25f089b14e79b527
Nurzhalgas Kurmangali <nurzhalgaskurmangali@gmail.com>
2022-11-30 07:06:46 +0600

    Network starting

--- app/src/main/java/com/example/reciper/ProfileViewModel.kt
+++ app/src/main/java/com/example/reciper/ProfileViewModel.kt
@@ -18,6 +18,39 @@ class ProfileViewModel : ViewModel(){
         Image("Images 4",
             R.drawable.png_clipart_eatsa_fast_food_restaurant_hamburger_choripan_leaf_vegetable_food
         ),
+        Image("Images 5", R.drawable.__2_junk_food_png_picture),
+        Image("Images 1", R.drawable.grilled_food_png_clipart_background),
+        Image("Images 2",
+            R.drawable.png_clipart_eatsa_fast_food_restaurant_hamburger_choripan_leaf_vegetable_food
+        ),
+        Image("Images 3",
+            R.drawable.png_clipart_eatsa_fast_food_restaurant_hamburger_choripan_leaf_vegetable_food
+        ),
+        Image("Images 4",
+            R.drawable.png_clipart_eatsa_fast_food_restaurant_hamburger_choripan_leaf_vegetable_food
+        ),
+        Image("Images 5", R.drawable.__2_junk_food_png_picture),
+        Image("Images 1", R.drawable.grilled_food_png_clipart_background),
+        Image("Images 2",
+            R.drawable.png_clipart_eatsa_fast_food_restaurant_hamburger_choripan_leaf_vegetable_food
+        ),
+        Image("Images 3",
+            R.drawable.png_clipart_eatsa_fast_food_restaurant_hamburger_choripan_leaf_vegetable_food
+        ),
+        Image("Images 4",
+            R.drawable.png_clipart_eatsa_fast_food_restaurant_hamburger_choripan_leaf_vegetable_food
+        ),
+        Image("Images 5", R.drawable.__2_junk_food_png_picture),
+        Image("Images 1", R.drawable.grilled_food_png_clipart_background),
+        Image("Images 2",
+            R.drawable.png_clipart_eatsa_fast_food_restaurant_hamburger_choripan_leaf_vegetable_food
+        ),
+        Image("Images 3",
+            R.drawable.png_clipart_eatsa_fast_food_restaurant_hamburger_choripan_leaf_vegetable_food
+        ),
+        Image("Images 4",
+            R.drawable.png_clipart_eatsa_fast_food_restaurant_hamburger_choripan_leaf_vegetable_food
+        ),
         Image("Images 5", R.drawable.__2_junk_food_png_picture)
     )
 
